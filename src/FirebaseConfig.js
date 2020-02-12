ae909d3c4c8e05ff3b1fb5ad6ab6407471c6fec2
mgangavarapu <mgangavarapu@in.imshealth.com>
2020-02-12 14:19:55 +0530

    adding firebase auth

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,12 +1,23 @@
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: "mj-tech-link",
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: "AIzaSyBEgAdrwXZC9DJrVELaa0uLJgMTX7rRT5c",
+  authDomain: "mj-tech-link.firebaseapp.com",
+  databaseURL: "https://mj-tech-link.firebaseio.com",
   projectId: "mj-tech-link",
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  storageBucket: "mj-tech-link.appspot.com",
+  messagingSenderId: "406397589059",
+  appId: "1:406397589059:web:ebaf9c0904904ae73466f6",
+  measurementId: "G-HNT2VTRY3D"
 };
 
 export default firebaseConfig;

36417d3e57ec1b439a05d4d910c07e9336aec52a
mgangavarapu <mgangavarapu@in.imshealth.com>
2020-02-12 11:59:00 +0530

    updating env

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,14 +1,14 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEgAdrwXZC9DJrVELaa0uLJgMTX7rRT5c",
-  authDomain: "mj-tech-link.firebaseapp.com",
-  databaseURL: "https://mj-tech-link.firebaseio.com",
-  projectId: "mj-tech-link",
-  storageBucket: "mj-tech-link.appspot.com",
-  messagingSenderId: "406397589059",
-  appId: "1:406397589059:web:ebaf9c0904904ae73466f6",
-  measurementId: "G-HNT2VTRY3D"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
